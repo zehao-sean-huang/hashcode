1c9ccb310bda46c4324eaabbacebc0dc7dccf00e
Zehao Huang <1078351005@qq.com>
2019-02-24 12:15:31 -0800

    reader

---
+++ pizza/reader.py
@@ -0,0 +1,24 @@
+def read(filename):
+    """
+    0 -- Tomato
+    1 -- Mushroom
+    """
+    try:
+        f = open(filename, "r")
+    except:
+        raise FileNotFoundError
+    r, c, l, h = tuple([int(parameter) for parameter in f.readline().split()])
+
+    pizza = []
+    used = []
+
+    for i in range(r):
+        pizza.append([0 if char == 'T' else 1 for char in f.readline()][:-1])
+        used.append([False for _ in pizza[i]])
+
+    # Debug
+    # print(pizza)
+    # print(used)
+    # End Debug
+
+    return r, c, l, h, pizza, used
