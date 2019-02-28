3d0e53d5a97d5828f91166a137f90e369b7c61a8
[Yijia Chen] <yijiachenmain@gmail.com>
2019-02-28 11:27:17 -0800

    Update scorer

--- competition/ByPartition.py
+++ competition/ByPartition.py
@@ -4,3 +4,4 @@ def find(filename):
 
     num_pic, orient, labels, label_pics = read_for_rank(filename)
     
+    
--- competition/scorer.py
+++ competition/scorer.py
@@ -1,7 +1,7 @@
-def score(order, labels):
+def score(order):
     total_score = 0
     for i in range(len(order) - 1):
-        p1, p2 = labels[order[i]], labels[order[i+1]]
+        p1, p2 = order[i][1], order[i+1][1]
         sc = 0 # number of common tags
         for k in range(len(p1)):
             if p1[k] in p2:
